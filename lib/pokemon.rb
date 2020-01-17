caeb430cdc9171f658fa4b6bf70c32e8bef4e7d3
Richard Lee <richlee81192@gmail.com>
2020-01-17 20:10:01 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,13 +9,13 @@ def initialize(id: nil, name:, type:, db: )
   @db = db
 end
 
-# def self.new_from_db(row)
-#   new_pokemon = Self.new
-#   new_pokemon.id = row[0]
-#   new_pokemon.name = row[1]
-#   new_pokemon.type = row[2]
-#   new_pokemon
-# end 
+def self.new_from_db(row)
+  new_pokemon = Self.new
+  new_pokemon.id = row[0]
+  new_pokemon.name = row[1]
+  new_pokemon.type = row[2]
+  new_pokemon
+end 
 
 def self.save(name, type, id )
   sql = <<-SQL
@@ -27,8 +27,8 @@ def self.save(name, type, id )
   @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
 end
 
-# def self.find
-#   sql = <<-SQL
+def self.find
+  sql = <<-SQL
 SELECT * 
 FROM pokemons
 WHERE name = ?
